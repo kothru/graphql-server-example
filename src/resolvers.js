b4a502bef843ed264a5296730cc0f31bb8c01e5a
kothru <1894683+kothru@users.noreply.github.com>
2022-03-24 23:31:18 +0900

    rm clg

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,8 +1,6 @@
 import pkg from '@prisma/client';
 const { PrismaClient } = pkg;
-
 const prisma = new PrismaClient()
-console.log(await prisma.user.findMany({ include: { posts: true } }))
 export const resolvers = {
   Query: {
     users: async () => await prisma.user.findMany({ include: { posts: true } })
